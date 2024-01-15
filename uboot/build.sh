1b95d3616bb23c0afe689602d31847d01942e9bb
Bruce.sui <sui2054131414@163.com>
2024-01-15 20:56:36 +0800

    add:修改uboot编译脚本

--- uboot/build.sh
+++ uboot/build.sh
@@ -1,6 +1,6 @@
  #!/bin/bash 
      
  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
- make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_evk_emmc_defconfig
+ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alpha_emmc_defconfig
  make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
 
