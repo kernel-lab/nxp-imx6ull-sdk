11f26a82bb82f8fa302ca2be3be79b206d31fee7
bruce <sui2054131414@163.com>
2024-01-21 13:58:43 +0800

    modify:完善build.sh编译脚本，加入clean参数

--- uboot/build.sh
+++ uboot/build.sh
@@ -1,6 +1,10 @@
- #!/bin/bash 
-     
- make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
- make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alpha_emmc_defconfig
- make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
+#!/bin/bash 
+
+if [ "$1" = "clean" ]; then
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+else
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alpha_emmc_defconfig
+    make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
+fi
 
