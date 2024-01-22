41a537ec2b1e8855728c6878b9e0655220cc4423
Bruce.sui <sui2054131414@163.com>
2024-01-22 17:18:42 +0800

    Add:添加linux kernel编译脚本

---
+++ imx-kernel/build_kernel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" = "clean" ]; then
+    echo "build clean linux kernel!!!"
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+elif [ "$1" = "menuconfig" ]; then
+    echo "build kernel menuconfig!!!"
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+else
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
+fi
+
