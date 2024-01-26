8b0d8d8c32e5c9f2df54b00cf3d1c5a78badf6dc
Bruce.sui <sui2054131414@163.com>
2024-01-26 11:45:42 +0800

    Modify:完善kernel编译脚本，添加编译提示语句start build kernel

--- imx-kernel/build_kernel.sh
+++ imx-kernel/build_kernel.sh
@@ -7,6 +7,7 @@ elif [ "$1" = "menuconfig" ]; then
     echo "build kernel menuconfig!!!"
     make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 else
+    echo "Start build kernel!!!"
     make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
     make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
     make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
