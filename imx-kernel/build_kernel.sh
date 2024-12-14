71e634a8005708f3d7090d5dd9cb1cfd310233f2
bruce.sui <bruce.sui@quectel.com>
2024-12-14 16:46:51 +0800

    fix:修复内核编译新增项目后的异常

--- imx-kernel/build_kernel.sh
+++ imx-kernel/build_kernel.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+PWDIR=$(pwd)
+
 if [ "$1" = "clean" ]; then
     echo -e "\033[31m>> build clean linux kernel!!!\033[0m"
     make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 elif [ "$1" = "menuconfig" ]; then
     echo -e "\033[31m>> build kernel menuconfig!!!\033[0m"
-    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alpha_emmc_defconfig menuconfig
+    cp $PWDIR/.config $PWDIR/arch/arm/configs/imx_alpha_emmc_defconfig
 else
+    if [ ! -f $PWDIR/.config ]; then
+        echo -e "\033[31m>> Linux kernel .config file not exist!!!\033[0m"
+        echo -e "\033[31m>> Please run: ./build_kernel.sh menuconfig\033[0m"
+        echo -e "\033[31m>> If you are in the SDK root directory.\033[0m"
+        echo -e "\033[31m>> Please run: ./build.sh kernel_menuconfig\033[0m"
+        echo -e "\033[31m>> Thanks\033[0m"
+        exit 1;
+    fi
     echo -e "\033[31m>> Start build kernel!!!\033[0m"
-    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alpha_emmc_defconfig
     make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
     echo -e "\033[32m########## build kernel success!!! #########\033[0m"
 fi
