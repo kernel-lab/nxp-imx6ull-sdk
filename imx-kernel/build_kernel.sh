5627c3961d9e47bb8c0ce7959e62326b18125f10
sui2054131414@163.com <sui2054131414@163.com>
2025-01-11 16:13:38 +0800

    style:优化内核编译脚本

--- imx-kernel/build_kernel.sh
+++ imx-kernel/build_kernel.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-PWDIR=$(pwd)
+set -e
 
-if [ "$1" = "clean" ]; then
-    echo -e "\033[31m>> build clean linux kernel!!!\033[0m"
-    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-elif [ "$1" = "menuconfig" ]; then
-    echo -e "\033[31m>> build kernel menuconfig!!!\033[0m"
-    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_alpha_emmc_defconfig menuconfig
-    cp $PWDIR/.config $PWDIR/arch/arm/configs/imx_alpha_emmc_defconfig
-else
-    if [ ! -f $PWDIR/.config ]; then
-        echo -e "\033[31m>> Linux kernel .config file not exist!!!\033[0m"
-        echo -e "\033[31m>> Please run: ./build_kernel.sh menuconfig\033[0m"
-        echo -e "\033[31m>> If you are in the SDK root directory.\033[0m"
-        echo -e "\033[31m>> Please run: ./build.sh kernel_menuconfig\033[0m"
-        echo -e "\033[31m>> Thanks\033[0m"
-        exit 1;
+RED='\033[31m'
+GREEN='\033[32m'
+RESET='\033[0m'
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+ARCH=arm
+CROSS_COMPILE=arm-linux-gnueabihf-
+
+case "$1" in
+    clean)
+        echo -e "${RED}>> Cleaning kernel!!!${RESET}"
+        make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+        ;;
+    menuconfig)
+        echo -e "${RED}>> Configuring kernel!!!${RESET}"
+        make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} imx_alpha_emmc_defconfig menuconfig
+
+        if [ -f "$SCRIPT_DIR/arch/arm/configs/imx_alpha_emmc_defconfig.bak" ]; then
+            mv "$SCRIPT_DIR/arch/arm/configs/imx_alpha_emmc_defconfig.bak" "$SCRIPT_DIR/arch/arm/configs/imx_alpha_emmc_defconfig.bak.old"
+        fi
+        cp "$SCRIPT_DIR/.config" "$SCRIPT_DIR/arch/arm/configs/imx_alpha_emmc_defconfig.bak"
+        cp "$SCRIPT_DIR/.config" "$SCRIPT_DIR/arch/arm/configs/imx_alpha_emmc_defconfig"
+        ;;
+esac
+
+if [ $# -eq 0 ]; then
+    if [ ! -f "$SCRIPT_DIR/.config" ]; then
+        echo -e "${RED}>> No .config found, creating default configuration!!!${RESET}"
+        make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} imx_alpha_emmc_defconfig
     fi
-    echo -e "\033[31m>> Start build kernel!!!\033[0m"
-    make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
-    echo -e "\033[32m########## build kernel success!!! #########\033[0m"
+    echo -e "${RED}>> Starting to build kernel!!!${RESET}"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all -j$(nproc)
+    echo -e "${GREEN}########## Build kernel successful!!! #########${RESET}"
 fi
-
